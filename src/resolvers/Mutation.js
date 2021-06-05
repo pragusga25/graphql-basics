2b4644efb63c7eafe328f9f91d23edf99fb32ee4
Taufik Pragusga <pragusga25@gmail.com>
2021-06-05 09:01:59 +0700

    Create updateUser mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -49,6 +49,25 @@ const Mutation = {
     return comment;
   },
 
+  updateUser(_, { id, data }, { db }) {
+    const user = db.users.find((user) => user.id === id);
+
+    if (!user) throw new Error('User not found');
+
+    if (typeof data.email === 'string') {
+      const emailTaken = db.users.some((user) => user.email === data.email);
+
+      if (emailTaken) throw new Error('Email taken');
+
+      user.email = data.email;
+    }
+
+    if (typeof data.name === 'string') user.name = data.name;
+
+    if (typeof data.age === 'undefined') user.age = data.age;
+
+    return user;
+  },
   deletePost(_, args, { db }) {
     const postIndex = db.posts.findIndex((post) => post.id === args.id);
 
