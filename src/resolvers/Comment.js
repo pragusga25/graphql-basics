8a4303016363f27bc51f5b8d0ece3d3517d5d20d
Taufik Pragusga <73622464+pragusga25@users.noreply.github.com>
2021-06-19 18:01:44 +0700

    Update Comment.js

--- src/resolvers/Comment.js
+++ src/resolvers/Comment.js
@@ -1,9 +1,9 @@
 const Comment = {
   author(parent, _, { db }) {
-    return db.users.find((user) => user.id === parent.authorId);
+    return db.users.find(user => user.id === parent.authorId);
   },
   post(parent, _, { db }) {
-    return db.posts.find((post) => post.id === parent.postId);
+    return db.posts.find(post => post.id === parent.postId);
   },
 };
 
